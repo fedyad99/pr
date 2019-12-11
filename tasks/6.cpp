6eeee67d8be9baa3d63c840509ad71553f6d0eaf
fedyad99 <55451100+fedyad99@users.noreply.github.com>
2019-12-11 21:15:26 +0300

    Update 6.cpp

--- tasks/6.cpp
+++ tasks/6.cpp
@@ -7,15 +7,24 @@ int main() {
 	double a, b, c;
 	cout << "Введите a,b,c" << endl;
 	cin >> a >> b >> c;
+	if (a + b + c == 0) cout << " 0 = 0, " << endl;
+	else if (a + b == 0) cout << " Вы ввели " << c << " = 0, это не уравнение" << endl;
+	else if (a + c == 0 || b + c == 0) {
+		cout << "X = 0" << endl;
+		return 0;
+	}
 	if (b * b - 4 * a * c < 0) {
 		cout << "Действительных корней нет" << endl;
-	} else if (b * b - 4 * a * c == 0) {
-		cout << "x = " << -b / (2*a) << endl;
-	} else if (a == 0) {
-		cout << "x = " << -b / c << endl;
-	  } else{
-			cout << "x1 = " << (-b + sqrt(b * b - 4 * a * c))/ (2 * a) << endl;
-			cout << "x2 = " << (-b - sqrt(b * b - 4 * a * c)) / (2 * a) << endl;
-	   }
+	}
+	else if (b * b - 4 * a * c == 0) {
+		cout << "x = " << -b / (2 * a) << endl;
+	}
+	else if (a == 0) {
+		cout << "x = " << -c / b << endl;
+	}
+	else {
+		cout << "x1 = " << (-b + sqrt(b * b - 4 * a * c)) / (2 * a) << endl;
+		cout << "x2 = " << (-b - sqrt(b * b - 4 * a * c)) / (2 * a) << endl;
+	}
 	return 0;
 }
