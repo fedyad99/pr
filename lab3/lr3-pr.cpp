5468969f998b7a42c7c27a6189f02962086e2cf8
fedyad99 <55451100+fedyad99@users.noreply.github.com>
2019-12-11 13:14:33 +0300

    Update lr3-pr.cpp

--- lab3/lr3-pr.cpp
+++ lab3/lr3-pr.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+#include "stdafx.h"
+#include <iostream>
 #include "libbmp.h"
 #include <string>
 using namespace std;
@@ -9,11 +10,11 @@ enum Channel {
 	BLUE
 };
 
-int binary_to_int(string t){
-	int result = (int)(t[0] - 48) * 128 + (int)(t[1] - 48) * 64 +
-		(int)(t[2] - 48) * 32 + (int)(t[3] - 48) * 16 +
-		(int)(t[4] - 48) * 8 + (int)(t[5] - 48) * 4 +
-		(int)(t[6] - 48) * 2 + (int)(t[7] - 48);
+int binary_to_int(string t) {
+	int result = (int)t[0] * 128 + (int)t[1] * 64 +
+		(int)t[2] * 32 + (int)t[3] * 16 +
+		(int)t[4] * 8 + (int)t[5] * 4 +
+		(int)t[6] * 2 + (int)t[7];
 	return result;
 }
 
@@ -26,16 +27,16 @@ bool check(string* temp) {
 	return true;
 }
 
-bool getChar(BmpImg* img, int x, int y, string* temp, Channel channel ) {
-	switch (channel){
-	case RED: 
-		*temp += ((int)img->red_at(x, y)) % 2 + 48;
+bool getChar(BmpImg* img, int x, int y, string* temp, Channel channel) {
+	switch (channel) {
+	case RED:
+		*temp += ((int)img->red_at(x, y)) % 2;
 		break;
-	case GREEN: 
-		*temp += ((int)img->green_at(x, y)) % 2 + 48;
+	case GREEN:
+		*temp += ((int)img->green_at(x, y)) % 2;
 		break;
-	case BLUE: 
-		*temp += ((int)img->blue_at(x, y)) % 2 + 48;
+	case BLUE:
+		*temp += ((int)img->blue_at(x, y)) % 2;
 		break;
 	}
 	if (!check(temp)) return false;
@@ -43,19 +44,21 @@ bool getChar(BmpImg* img, int x, int y, string* temp, Channel channel ) {
 }
 
 
-int main(){
+int main() {
 	setlocale(LC_ALL, "Russian");
 	BmpImg img;
 	img.read("pic2.bmp");
 	string temp = "";
 
 	for (int i = 0; i < img.get_height(); i++) {         // KEY  =  00r 00g 00b 10r 10g 10b 01r 01g
-		for (int j = 0; j < img.get_width(); j++) {   
+		for (int j = 0; j < img.get_width(); j++) {
 			if (!getChar(&img, j, i, &temp, RED)) return 1;
 			if (!getChar(&img, j, i, &temp, GREEN)) return 2;
-			if (!getChar(&img, j, i, &temp, BLUE)) return 3;
+			if (!getChar(&img, j, i, &temp, BLUE)) {
+				system("pause"); return 3;
+			}
 		}
 	}
-
+	system("pause");
 	return 4;
 }
