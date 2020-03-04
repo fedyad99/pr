86922dca45aa627d0effb9280a8fc4fe6bad6f16
fedyad99 <55451100+fedyad99@users.noreply.github.com>
2020-03-04 23:26:25 +0300

    Add files via upload

---
+++ tasks/T10.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+double Step(double a, double b)
+{
+	if (b < 2) return a;
+	return Step(a, b - 1)*a;
+}
+
+int main()
+{
+	double a, b;
+
+	std::cin >> a >> b;
+	std::cout << Step(a, b);
+}
