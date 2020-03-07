12316f41d6165cf5b72db7757f9c5f920762151c
fedyad99 <55451100+fedyad99@users.noreply.github.com>
2020-03-07 17:39:39 +0300

    Update T31.cpp

--- tasks/T31.cpp
+++ tasks/T31.cpp
@@ -1,6 +1,5 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
-#include <stack>
 
 std::string maze[25] = {
 "####B######################",
@@ -71,36 +70,22 @@ void AddExit(char exit)
 	Sort();
 }
 
-void Search(std::stack<Cor> * Stack) 
+void Search(Cor cor) 
 {
-	if (!Stack->empty()) 
-	{
-		Cor cor = Stack->top();
-		if (isalpha(maze[cor.y][cor.x])) { AddExit(maze[cor.y][cor.x]); return; }
+
+		if (isalpha(maze[cor.y][cor.x])) 
+		{ 
+			AddExit(maze[cor.y][cor.x]); 
+			return; 
+		}
 		maze[cor.y][cor.x] = '#';
 
-		if (maze[cor.y + 1][cor.x] != '#') 
-		{
-			Stack->push(Cor{ cor.x, cor.y + 1 });
-			Search(Stack);
-		}
-		if (maze[cor.y - 1][cor.x] != '#') 
-		{
-			Stack->push(Cor{ cor.x, cor.y - 1 });
-			Search(Stack);
-		}
-		if (maze[cor.y][cor.x + 1] != '#') 
-		{
-			Stack->push(Cor{ cor.x + 1, cor.y});
-			Search(Stack);
-		}
-		if (maze[cor.y][cor.x - 1] != '#') 
-		{
-			Stack->push(Cor{ cor.x - 1, cor.y });
-			Search(Stack);
-		}
-		Stack->pop();
-	}
+		if (maze[cor.y + 1][cor.x] != '#') Search(Cor{ cor.x, cor.y + 1 });
+		if (maze[cor.y - 1][cor.x] != '#') Search(Cor{ cor.x, cor.y - 1 });
+		if (maze[cor.y][cor.x + 1] != '#') Search(Cor{ cor.x + 1, cor.y });
+		if (maze[cor.y][cor.x - 1] != '#') Search(Cor{ cor.x - 1, cor.y });
+	
+
 }
 
 int main()
@@ -111,10 +96,7 @@ int main()
 	std::cin >> Coordinates.x >> Coordinates.y;
 	if (Check(Coordinates)) return 0;
 
-	std::stack<Cor> Stack;
-	Stack.push(Coordinates);
-
-	Search(&Stack);
+	Search(Coordinates);
 
 	if (ExitLab.size() == 0) std::cout << "Выхода нет" << std::endl;
 	else for (int i = 0; i < ExitLab.size(); i++) std::cout << ExitLab[i] << " ";
