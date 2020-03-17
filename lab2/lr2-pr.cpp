6ce58f1e12e902938b0aab501b90a02a44f35646
fedyad99 <55451100+fedyad99@users.noreply.github.com>
2020-03-18 01:01:33 +0300

    Update lr2-pr.cpp

--- lab2/lr2-pr.cpp
+++ lab2/lr2-pr.cpp
@@ -1,6 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>
 using namespace std;
+
+bool Equal(double a, double b, double epsilon)
+{
+	return fabs(a - b) <= ((fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
 
 int main(){
 	setlocale(LC_ALL,"Russian");
@@ -8,21 +13,21 @@ int main(){
 	cout << "Введите a, b, xn, xk, dx" << endl;
 	cin >> a >> b >> xn >> xk >> dx;
 	double y, min, max;
-	for (double x = xn; x <= xk; x += dx) {
+	for (double x = xn; x < xk || Equal(x, xk, 0.00001); x += dx) {
 		
-		if (x <= a) {
+		if (x < a || Equal(x, a, 0.00001)) {
 			y = exp(x);
 		}
 		else if (x < b) {
 			y = exp(x) + cos(x);
 		}
 		else {
-			if (cos(x) != 0) {
+			if ( Equal( cos(x), 0, 0.0001)) {
 				y = tan(x);
 			}
 			else cout << "При x = " << x << " корней нет" << endl;
 		}
-		if (x == xn) {
+		if (Equal(x, xn, 0.00001)) {
 			min = y;
 			max = y;
 		}
